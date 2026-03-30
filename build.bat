f04fa77a1b6354842dd22da8d5e0865248597962
LxnChan <lingxuanning@outlook.com>
2026-03-30 13:52:26 +0800

    update build batch

--- build.bat
+++ build.bat
@@ -1,13 +1,18 @@
-set GOOS=
-set GOARCH=
+@echo off
+
+echo Build for Windows amd64
+set GOOS=windows
+set GOARCH=amd64
 set CGO_ENABLED=
 go build -o release/onedrivecli-windows-amd64.exe ./cmd/onedrivecli
 
+echo Build for Linux amd64
 set GOOS=linux
 set GOARCH=amd64
 set CGO_ENABLED=0
 go build -o release/onedrivecli-linux-amd64 ./cmd/onedrivecli
 
+echo Build for Linux arm64
 set GOOS=linux
 set GOARCH=arm64
 set CGO_ENABLED=0
